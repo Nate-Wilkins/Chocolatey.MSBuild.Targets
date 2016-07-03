36633ab27f3f339289c1e2b9aecc67b959599751
Nate-Wilkins <nwilkins2012@gmail.com>
2016-07-03 00:25:37 -0400

    feat(chocolatey_pack): add pack build target

---
+++ Tools/install.ps1
@@ -0,0 +1,3 @@
+﻿# Install Chocolatey
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+[Environment]::SetEnvironmentVariable("Path", "$env:Path;$env:ALLUSERSPROFILE\chocolatey\bin", [EnvironmentVariableTarget]::Machine)
