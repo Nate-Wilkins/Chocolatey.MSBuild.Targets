3553ca26e22d9dc22c500d2517208fa30c3b47d6
Nate-Wilkins <nwilkins2012@gmail.com>
2016-07-03 00:59:49 -0400

    fix(install.ps1): unable to install chocolatey
    security issues need to be fixed

--- Tools/install.ps1
+++ Tools/install.ps1
@@ -1,3 +1,3 @@
-﻿# Install Chocolatey
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-[Environment]::SetEnvironmentVariable("Path", "$env:Path;$env:ALLUSERSPROFILE\chocolatey\bin", [EnvironmentVariableTarget]::Machine)
+﻿# TODO: Install Chocolatey
+#powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+#[Environment]::SetEnvironmentVariable("Path", "$env:Path;$env:ALLUSERSPROFILE\chocolatey\bin", [EnvironmentVariableTarget]::Machine)
